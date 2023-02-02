c92fb743f5a70567b6a64996c682e026fe07bc76
kuca7a <jakubkucala@gmail.com>
2023-02-02 13:17:51 +0000

    design and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BlogComponent } from './blog/blog.component';
+import { EventsComponent } from './events/events.component';
+import { FaqComponent } from './faq/faq.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { OurteamComponent } from './ourteam/ourteam.component';
+import { PayComponent } from './pay/pay.component';
+
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'blog', component: BlogComponent},
+  {path:'events', component: EventsComponent},
+  {path:'gallery', component: GalleryComponent},
+  {path:'pay', component: PayComponent},
+  {path:'faq', component: FaqComponent},
+  {path:'ourteam', component: OurteamComponent},
+  {path:'login', component: LoginComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,  {enableTracing: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { BlogComponent } from './blog/blog.component';
+import { EventsComponent } from './events/events.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { PayComponent } from './pay/pay.component';
+import { FaqComponent } from './faq/faq.component';
+import { OurteamComponent } from './ourteam/ourteam.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    BlogComponent,
+    EventsComponent,
+    GalleryComponent,
+    PayComponent,
+    FaqComponent,
+    OurteamComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
